3ebf7baf0f8a6d71b76efec73dc8b7728d870dc9
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-19 19:39:12 +0100

    function switch

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,12 @@ import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
 import AllTheBooks from "./components/AllTheBooks";
-import ButtonComponent from "./components/ButtonComponent";
 
 function App() {
     return (
         <div className="App">
             <MyNav home="home" about="Abous Us" browse="Find Us" />
             <Welcome welcome="Benvenuto nella libreria super Top!" />
-            <ButtonComponent theme="danger" Btncontent="Fantasy" />
-            <ButtonComponent theme="info" Btncontent="Horror" />
-            <ButtonComponent theme="success" Btncontent="History" />
-            <ButtonComponent theme="primary" Btncontent="Romance" />
-            <ButtonComponent theme="light" Btncontent="Sci-fi" />
             <AllTheBooks />
             <MyFooter testo_par="questo è del testo passato tramite props" />
         </div>
--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,26 +1,92 @@
-import { Component } from "react";
-/* import fantasyBooks from "../data/fantasy.json";
-import horrorBook from "../data/horror.json"; */
-/* import RomanceBook from "../data/romance.json";
-import HistoryBook from "../data/history.json"; */
-import sciFiBook from "../data/scifi.json";
+import React, { Component } from "react";
+import fantasyBooks from "../data/fantasy.json";
+import horrorBooks from "../data/horror.json";
+import romanceBooks from "../data/romance.json";
+import historyBooks from "../data/history.json";
+import sciFiBooks from "../data/scifi.json";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import ButtonComponent from "./ButtonComponent";
 
 class AllTheBooks extends Component {
+    state = {
+        books: sciFiBooks,
+        currentCategory: "Sci-fi", // Add currentCategory to the state
+    };
+
+    changeCategory = (category) => {
+        let newBooks;
+
+        switch (category) {
+            case "Fantasy":
+                newBooks = fantasyBooks;
+                break;
+            case "Horror":
+                newBooks = horrorBooks;
+                break;
+            case "History":
+                newBooks = historyBooks;
+                break;
+            case "Romance":
+                newBooks = romanceBooks;
+                break;
+            case "Sci-fi":
+                newBooks = sciFiBooks;
+                break;
+            default:
+                newBooks = sciFiBooks; // Default to Sci-fi
+        }
+
+        this.setState({
+            currentCategory: category,
+            books: newBooks,
+        });
+    };
+
     render() {
+        const { books } = this.state;
+
         return (
             <Container>
                 <Row>
-                    {sciFiBook.map((book, index) => (
+                    <div>
+                        <ButtonComponent
+                            theme="danger"
+                            Btncontent="Fantasy"
+                            onClick={() => this.changeCategory("Fantasy")}
+                        />
+                        <ButtonComponent
+                            theme="info"
+                            Btncontent="Horror"
+                            onClick={() => this.changeCategory("Horror")}
+                        />
+                        <ButtonComponent
+                            theme="success"
+                            Btncontent="History"
+                            onClick={() => this.changeCategory("History")}
+                        />
+                        <ButtonComponent
+                            theme="primary"
+                            Btncontent="Romance"
+                            onClick={() => this.changeCategory("Romance")}
+                        />
+                        <ButtonComponent
+                            theme="warning"
+                            Btncontent="Sci-fi"
+                            onClick={() => this.changeCategory("Sci-fi")}
+                        />
+                    </div>
+
+                    {books.map((book) => (
                         <Col key={`book-${book.asin}`} xs={3} className="my-2 p-3">
                             <Card className="h-100">
                                 <Card.Img variant="top" src={book.img} className="img-format" />
                                 <Card.Body className="h-auto d-flex flex-column justify-content-center">
                                     <Card.Title>{book.title}</Card.Title>
                                     <Card.Text className="fs-3 text-primary">{book.price}$</Card.Text>
+                                    <ButtonComponent theme="success" Btncontent="Add To Cart" />
                                 </Card.Body>
                             </Card>
                         </Col>
